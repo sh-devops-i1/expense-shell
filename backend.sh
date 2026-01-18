05fef3785da7a0c534bd64a76ded7970cf402a30
shujath-git900 <shujath.devops@gmail.com>
2026-01-18 13:53:57 +0530

    output of command redirected to file

--- backend.sh
+++ backend.sh
@@ -1,16 +1,17 @@
 set -x
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+dnf module disable nodejs -y > /tmp/expense.log
+dnf module enable nodejs:20 -y > /tmp/expense.log
+dnf install nodejs -y > /tmp/expense.log
 useradd expense
 cp backend.service /etc/systemd/system/backend.service
 mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip > /tmp/expense.log
 
 cd /app
-unzip /tmp/backend.zip
-npm install
+unzip /tmp/backend.zip > /tmp/expense.log
+npm install > /tmp/expense.log
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
+systemctl daemon-reload > /tmp/expense.log
+systemctl enable backend > /tmp/expense.log
+systemctl start backend > /tmp/expense.log
+
