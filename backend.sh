db15a33592b813dfd906136eec22866351f235a6
shujath-git900 <shujath.devops@gmail.com>
2026-01-18 13:56:37 +0530

    added &>> to append the output

--- backend.sh
+++ backend.sh
@@ -1,17 +1,17 @@
 set -x
-dnf module disable nodejs -y > /tmp/expense.log
-dnf module enable nodejs:20 -y > /tmp/expense.log
-dnf install nodejs -y > /tmp/expense.log
+dnf module disable nodejs -y &>> /tmp/expense.log
+dnf module enable nodejs:20 -y &>> /tmp/expense.log
+dnf install nodejs -y &>> /tmp/expense.log
 useradd expense
 cp backend.service /etc/systemd/system/backend.service
 mkdir /app
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip > /tmp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>> /tmp/expense.log
 
 cd /app
-unzip /tmp/backend.zip > /tmp/expense.log
-npm install > /tmp/expense.log
+unzip /tmp/backend.zip &>> /tmp/expense.log
+npm install &>> /tmp/expense.log
 
-systemctl daemon-reload > /tmp/expense.log
-systemctl enable backend > /tmp/expense.log
-systemctl start backend > /tmp/expense.log
+systemctl daemon-reload &>> /tmp/expense.log
+systemctl enable backend &>> /tmp/expense.log
+systemctl start backend &>> /tmp/expense.log
 
