cedcaa5bc1948e0fe5bda48c7e566f156f70b987
shujath-git900 <shujath.devops@gmail.com>
2026-01-20 15:24:02 +0530

    added exit status in all 3 components

--- backend.sh
+++ backend.sh
@@ -2,42 +2,53 @@ source common.sh
 
 print_Head "Disable nodejs"
 dnf module disable nodejs -y &>> $LOG
+echo $?
 
 print_Head "Enable nodejs"
 dnf module enable nodejs:20 -y &>> $LOG
+echo $?
 
 print_Head "Install nodejs"
 dnf install nodejs -y &>> $LOG
+echo $?
 
 print_Head "Add user"
 useradd expense
+echo $?
 
 print_Head "Copy service file to Systemd"
 cp backend.service /etc/systemd/system/backend.service
+echo $?
 
 print_Head "Creating Directory"
 mkdir /app
+echo $?
 
 rm -rf /app/*
 print_Head "Download App content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>> $LOG
+echo $?
 
 print_Head "Unzip app content"
 cd /app
-
 unzip /tmp/backend.zip &>> $LOG
+echo $?
 
 print_Head "install nodejs dependencies npm package"
 npm install &>> $LOG
+echo $?
 
 print_Head "Starting Backend.."
 systemctl daemon-reload &>> $LOG
 systemctl enable backend &>> $LOG
 systemctl start backend &>> $LOG
+echo $?
 
 print_Head "Install mysql client"
 dnf install mysql -y &>> $LOG
+echo $?
 
 print_Head "Setup Schema"
 mysql -h 172.31.31.217 -uroot -p${mysql_root_password} < /app/schema/backend.sql
+echo $?
 
--- frontend.sh
+++ frontend.sh
@@ -2,17 +2,26 @@
 source common.sh
 print_Head "Install Nginx"
 dnf install nginx -y &>>$LOG
+echo $?
 
 print_Head "copy nginx Reverse proxy conf file"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
+echo $?
+
 print_Head "Remove Old content"
 rm -rf /usr/share/nginx/html/* &>>$LOG
+echo $?
+
 print_Head "Download App content"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$LOG
+echo $?
+
 print_Head "Unzip the content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG
+echo $?
 
 print_Head "Start Nginx"
 systemctl enable nginx &>>$LOG
 systemctl start nginx &>>$LOG
+echo $?
