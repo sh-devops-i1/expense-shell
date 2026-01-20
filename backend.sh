c2df5feff3df1686457c03f9ee564ffdf848b0ba
shujath-git900 <shujath.devops@gmail.com>
2026-01-20 14:41:42 +0530

    quotes fixed

--- backend.sh
+++ backend.sh
@@ -2,7 +2,7 @@ mysql_root_password=$1
 
 print_Head (){
   echo $1
-  echo "################## $1 ################# &>>/tmp/expense.log"
+  echo "################## $1 #################" &>>/tmp/expense.log
 }
 
 print_Head "Disable nodejs"
--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 print_Head (){
   echo $1
-  echo "################## $1 ################# &>>/tmp/expense.log"
+  echo "################## $1 #################" &>>/tmp/expense.log
 }
 
 mysql_root_password=$1
